6e44f6a72b7f20c7d548b8b7bfd2b5aa9183242f
comeback01 <comeback01@gmail.com>
2024-04-17 19:57:45 +0200

    Update index.tsx

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -16,7 +16,7 @@ const HeaderBar = () => {
         <div className={styles.logoBar}>
           <Link href="/">
             <img alt="logo" src="/logo192.png" />
-            <h1>WebiScriptura</h1>
+            <h1>WebiScriptura - 22.04.2024</h1>
           </Link>
         </div>
         <Space className={styles.right} size={0}>
